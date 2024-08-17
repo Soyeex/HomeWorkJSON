ce09383ff40f5619648b29d9f7ff9ef28a9821c8
Soyeex <p.mikkoev@bk.ru>
2024-08-17 20:16:28 +0300

    Create HW_data.js

---
+++ HW_data.js
@@ -0,0 +1,42 @@
+const userTable = `
+{
+    "users": [
+        {
+            "name": "John",
+            "age": 30,
+            "position": 1,
+            "img": "https://avatars.mds.yandex.net/i?id=ed1201e11a9a416a313a0963a4940e71dac8148a-5330362-images-thumbs&n=13"
+            
+        },
+        {
+            "name": "Jane",
+            "age": 25,
+            "position": 2,
+            "img": "https://cdn.onlinewebfonts.com/svg/img_507444.png"
+        },
+        {
+            "name": "Bob",
+            "age": 40,
+            "position": 3,
+            "img": "https://avatars.mds.yandex.net/i?id=ed1201e11a9a416a313a0963a4940e71dac8148a-5330362-images-thumbs&n=13"
+        },
+        {
+            "name": "Alice",
+            "age": 35,
+            "position": 4,
+            "img": "https://cdn.onlinewebfonts.com/svg/img_507444.png"
+        },
+        {
+            "name": "Mark", 
+            "age": 28,
+            "position": 5,
+            "img": "https://avatars.mds.yandex.net/i?id=ed1201e11a9a416a313a0963a4940e71dac8148a-5330362-images-thumbs&n=13"
+        },
+        {
+            "name": "Kate",
+            "age": 32,
+            "position": 6,
+            "img": "https://cdn.onlinewebfonts.com/svg/img_507444.png"
+        }
+    ]
+}`
